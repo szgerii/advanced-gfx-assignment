c12ff2c04cc5c5a887b3ee05f1c6f0c9fdeeeef2
szgeri <szucsi1104@gmail.com>
2026-06-01 04:19:19 +0200

    Initial project setup

---
+++ include/vendor.h
@@ -0,0 +1,13 @@
+// CENTRAL POINT FOR GRAPHICS VENDOR INCLUDES
+// because inclusion order can be sensitive for glad/glfw, and they behaviour-modifying defines can
+// easily get scattered across the codebase, inclusion should only be done via this header
+
+#pragma once
+
+#include <glad/gl.h>
+
+#include <GLFW/glfw3.h>
+
+#ifdef __cplusplus // glm imports <cmath>, breaking C compilation
+    #include <glm/glm.hpp>
+#endif
